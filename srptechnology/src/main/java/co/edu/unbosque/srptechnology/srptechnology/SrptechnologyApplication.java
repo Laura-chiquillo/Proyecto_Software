416f810803d935c6517cf6977eebcae009d38e34
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-10-31 13:08:16 -0500

    inicio

---
+++ srptechnology/src/main/java/co/edu/unbosque/srptechnology/srptechnology/SrptechnologyApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.srptechnology.srptechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrptechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrptechnologyApplication.class, args);
+	}
+
+}
---
+++ srptechnology/src/test/java/co/edu/unbosque/srptechnology/srptechnology/SrptechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.srptechnology.srptechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrptechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
