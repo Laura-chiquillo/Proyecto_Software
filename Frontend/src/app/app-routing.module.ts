ee481e32ce8282a10711fe3383b1cb8188d34666
Ricardo-rgb <61804423+Ricardo-rgb@users.noreply.github.com>
2021-11-13 19:08:11 -0500

    con nueva version

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./components/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./components/auth/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./components/pages/about/about.module').then(
+        (m) => m.AboutModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
