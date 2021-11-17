21fafaa2f9493aa13c62ef395b2eabd24fb3b52d
Ricardo-rgb <61804423+Ricardo-rgb@users.noreply.github.com>
2021-11-17 02:05:16 -0500

    Avance de validacion de registro
    Se cambio tsconfig.json , ademas se realizo una reestructuracion de la pagina web para que pueda cumplirse el fromGroup, algunas validaciones, tiene un bug con implementar la validacion required en los Select se probara con otro tipos en la siguiente version

--- Frontend/src/app/components/auth/registro/registro.component.ts
+++ Frontend/src/app/components/auth/registro/registro.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { TipoUsuarioService } from 'src/app/service/TipoUsuarioService';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class RegistroComponent {
+  public registroForm: FormGroup;
+
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  constructor(private router:Router, private service:TipoUsuarioService) {
+    this.registroForm = this.createForm();
+   }
+   get nombre() {return this.registroForm.get('nombre');}
+   get apellido() {return this.registroForm.get('apellido');}
+   get telefono() {return this.registroForm.get('telefono');}
+   get email() {return this.registroForm.get('email');}
+   get contrasena() {return this.registroForm.get('contrasena');}
+   get confContrasena() {return this.registroForm.get('confContrasena');}
+   get tDocumento() {return this.registroForm.get('tDocumento');}
+   get nDocumento() {return this.registroForm.get('nDocumento');}
+   get tUsuario() {return this.registroForm.get('tUsuario');}
+   get funcionalidad() {return this.registroForm.get('funcionalidad');}
+   get sexo() {return this.registroForm.get('sexo');}
+
+  createForm(){
+    return new FormGroup({
+      nombre: new FormControl('',[Validators.required, Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(this.emailPattern)]),
+      apellido: new FormControl('',[Validators.required, Validators.minLength(5)]),
+      telefono: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[- +()0-9]+')]),
+      contrasena: new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')]),
+      confContrasena: new FormControl('',[Validators.required,Validators.minLength(8)]),
+      tDocumento: new FormControl('',[Validators.required]),
+      nDocumento: new FormControl('',[Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[- +()0-9]+')]),
+      funcionalidad: new FormControl('',[Validators.required]),
+      sexo: new FormControl('',[Validators.required])
+    })
+  }
+  onResetForm(): void{
+    this.registroForm.reset();
   }
 
+  onSaveForm():void {
+    
+  }
+  
 }
