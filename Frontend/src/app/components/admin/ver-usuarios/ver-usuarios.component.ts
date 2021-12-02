152091c3c3f9ab74511621b8117ea13779c0fad6
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2021-12-02 14:32:28 -0500

    generar pdf en tablaUsuario

--- Frontend/src/app/components/admin/ver-usuarios/ver-usuarios.component.ts
+++ Frontend/src/app/components/admin/ver-usuarios/ver-usuarios.component.ts
@@ -3,8 +3,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ListaUsiarioService } from 'src/app/service/ListaUsuarioService';
 import { Usuario } from 'src/app/entity/Usuario';
-import {PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
+import { PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
 import * as pdfFonts from "pdfmake/build/vfs_fonts";
+
 PdfMakeWrapper.setFonts(pdfFonts);
 const pdf = new PdfMakeWrapper;
 
@@ -13,6 +14,7 @@ const pdf = new PdfMakeWrapper;
   templateUrl: './ver-usuarios.component.html',
   styleUrls: ['./ver-usuarios.component.css']
 })
+
 export class VerUsuariosComponent implements OnInit {
   usuario: Usuario = new Usuario();
   category: string;
@@ -20,6 +22,7 @@ export class VerUsuariosComponent implements OnInit {
   datos: any;
   generarPDF(): void {
 
+    // PDF
     pdf.info({
       title: 'Reporte de Usuarios registrados',
       subject: 'Se mostrara la cantida de usuarios que existen en la base de datos'
@@ -28,21 +31,34 @@ export class VerUsuariosComponent implements OnInit {
     pdf.styles({
       centrado: {
         alignment: 'center'
-      }
-    });
-    pdf.footer(new Txt('SRP technology').color('blue').fontSize(18).alignment('right').end)
-    //No agarra la informacion dentro de la tabla quien sea que lea esto se lo encargo :'v//
-    new Table([
-      this.displayedColumns, this.datos
-    ]).end
-    pdf.add(Table)
-    pdf.add(new Txt('Reportes de Usuarios').alignment('center').bold().fontSize(24).end)
-    pdf.add(pdf.ln(3))
-    pdf.create().open();
+      },
 
+    });
+
+    pdf.add(new Txt('Reportes de Usuarios').alignment('center').bold().fontSize(24).end)
+    const tabla = [this.TablaPdf]
+
+    this.datos.forEach(i => {
+      let fila = []
+      for (let key in i) {
+        // se ignora para que no aparezca en la tabla
+        if (key != "contrasena_emp")
+          fila.push(i[key])
+
+      }
+      tabla.push(fila)
+    })
+    // witdths funciona con la cantidad de columnas
+    pdf.add(new Table(tabla).widths(['auto','auto','auto','auto','auto','auto','auto','auto','auto','auto','auto']).fontSize(10).alignment('center')
+    .margin(5).end)
+    pdf.pageOrientation("landscape")
+    pdf.footer(new Txt('SRP technology').color('blue').fontSize(18).alignment('center').end)
+    pdf.create().open();
 
   }
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
   constructor(public service: ListaUsiarioService) {
 
   }
@@ -51,6 +67,11 @@ export class VerUsuariosComponent implements OnInit {
     'sexo_emp', 'telefono_emp', 'id_nivel', 'id_fun', 'estado_emp',
     'contrasena_emp', 'tipo_documento_emp', 'acciones'];
   dataSource = new MatTableDataSource<any>();
+
+  // se crea para mostrar en el pdf los titulos
+  TablaPdf: string[] = ['id', 'nombres',
+    'apellidos', 'numero:', 'correo',
+    'género', 'telefono', 'nivel', 'función', 'estado', 'documento'];
 
   ngOnInit(): void {
     this.service.loginAut().subscribe(data => {
@@ -116,3 +137,11 @@ export class VerUsuariosComponent implements OnInit {
   }
 
 }
+function createTable(data: any, displayedColumns: string[]) {
+  throw new Error('Function not implemented.');
+}
+
+function data(data: any, displayedColumns: string[]) {
+  throw new Error('Function not implemented.');
+}
+
